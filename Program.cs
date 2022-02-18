7a8cc1cc467220c4f6d2cf8fe87df4c26e9b1326
AliDiaa0 <85357930+AliDiaa0@users.noreply.github.com>
2022-02-19 00:59:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AboutTON618
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press the enter key to view facts about the ultramassive black hole TON 618!");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("1. TON 618 is the largest known black hole in the observable universe!");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("2. TON 618 is a hyperluminous quasar!");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("3. TON 618's mass is 66,000,000,000 solar-masses!");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("4. The Schwarzschild radius of the black hole TON 618 is 194,965,925,905 kilometers!");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("5. The quasar TON 618 is about 140,000,000,000,000 times brighter than the Sun!");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("6. The black hole TON 618 is about 10,370,000,000 light-years away from the Earth!");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Thanks for reading these cute facts the ultramassive black hole TON 618!");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("And now, TON 618 is saying (Thank you for reading about me, user! :D)");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+        }
+    }
+}
